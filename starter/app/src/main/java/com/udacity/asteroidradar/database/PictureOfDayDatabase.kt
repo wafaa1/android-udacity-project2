7c29c9055db221cfa8a18abdd25dfd5d7ebd7bea
@wafaa1 <wafaaegypt@yahoo.com>
2022-06-05 07:27:41 -0500

    WIP: working on POD repo

--- starter/app/src/main/java/com/udacity/asteroidradar/database/PictureOfDayDatabase.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/database/PictureOfDayDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [DatabaseAsteroid::class], version = 1)
+@Database(entities = [DatabasePOD::class], version = 1)
 abstract class PictureOfDayDatabase : RoomDatabase() {
     abstract val pictureOfDayDao: PictureOfDayDao
 }
