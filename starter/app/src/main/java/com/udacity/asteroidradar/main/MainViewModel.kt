92706965d39ab636b563214e593068430499f891
@wafaa1 <wafaaegypt@yahoo.com>
2022-06-05 07:19:02 -0500

    WIP: working on pod repo

--- starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -3,20 +3,27 @@ package com.udacity.asteroidradar.main
 import android.app.Application
 import androidx.lifecycle.*
 import com.udacity.asteroidradar.database.getAsteroidsDatabase
+import com.udacity.asteroidradar.database.getPODDatabase
 import com.udacity.asteroidradar.repository.AsteroidRepository
+import com.udacity.asteroidradar.repository.PODRepository
 import kotlinx.coroutines.launch
 
 class MainViewModel(application: Application) : AndroidViewModel(application){
     private val asteroidsDatabase = getAsteroidsDatabase(application)
     private val asteroidsRepository = AsteroidRepository(asteroidsDatabase)
 
+    private val podDatabase = getPODDatabase(application)
+    private val podRepository = PODRepository(podDatabase)
+
     init {
         viewModelScope.launch {
             asteroidsRepository.refreshAsteroids()
+            podRepository.refreshPictureOfDay()
         }
     }
 
     val asteroidsList = asteroidsRepository.asteroids
+    val pictureOfDay = podRepository.pictureOfDay
 }
 
 
---
+++ starter/app/src/main/java/com/udacity/asteroidradar/repository/PODRepository.kt
@@ -0,0 +1,37 @@
+package com.udacity.asteroidradar.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import com.udacity.asteroidradar.api.*
+import com.udacity.asteroidradar.database.AsteroidsDatabase
+import com.udacity.asteroidradar.database.PictureOfDayDatabase
+import com.udacity.asteroidradar.database.asDomainModel
+import com.udacity.asteroidradar.domain.Asteroid
+import com.udacity.asteroidradar.domain.PictureOfDay
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
+
+class PODRepository (private val database: PictureOfDayDatabase) {
+
+    private val startTodayDate = getTodayDate()
+    private val endAfter7Days = getDefaultEndDate()
+
+    val pictureOfDay: LiveData<PictureOfDay> =
+        Transformations.map(database.pictureOfDayDao.getPicOfDay(getTodayDate()))
+
+    suspend fun refreshPictureOfDay() {
+        withContext(Dispatchers.IO) {
+            val pic = PictureApi.apod.getPictureOfDay()
+            database
+                .pictureOfDayDao.insertAll()
+        }
+    }
+
+    suspend fun deleteOldPOD() {
+        withContext(Dispatchers.IO) {
+            database.pictureOfDayDao.clearOldPOD(getTodayDate())
+        }
+
+    }
+}
