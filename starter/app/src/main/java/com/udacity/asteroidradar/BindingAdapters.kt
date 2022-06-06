e6a79d9e2541e53a138b05e3761ea5d566d955d4
@wafaa1 <wafaaegypt@yahoo.com>
2022-06-06 10:06:17 -0500

    WIP working on bindingAdapters

--- starter/app/src/main/java/com/udacity/asteroidradar/BindingAdapters.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/BindingAdapters.kt
@@ -5,6 +5,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.udacity.asteroidradar.main.MainViewModel
@@ -26,6 +27,17 @@ fun bindDetailsStatusImage(imageView: ImageView, isHazardous: Boolean) {
         imageView.setImageResource(R.drawable.asteroid_safe)
     }
 }
+/// added changes per hazardous or not
+@BindingAdapter("asteroidContentDescription")
+fun bindAsteroidContentDescription(imageView: ImageView, isHazardous: Boolean) {
+    if (isHazardous) {
+        imageView.contentDescription =
+            imageView.context.getString(R.string.potentially_hazardous_asteroid_image)
+    } else {
+        imageView.contentDescription =
+            imageView.context.getString(R.string.not_hazardous_asteroid_image)
+    }
+}
 
 @BindingAdapter("astronomicalUnitText")
 fun bindTextViewToAstronomicalUnit(textView: TextView, number: Double) {
@@ -44,26 +56,33 @@ fun bindTextViewToDisplayVelocity(textView: TextView, number: Double) {
     val context = textView.context
     textView.text = String.format(context.getString(R.string.km_s_unit_format), number)
 }
-
+////
 @BindingAdapter("pictureOfDayUrl")
 fun bindImage(imgView: View, imgUrl: String?){
 }
 
 
-/////////////////
-@BindingAdapter("imageUrl")
-fun bindImage(imgView: ImageView, imgUrl: String?) {
-    imgUrl?.let {
-        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
-        Glide.with(imgView.context)
-            .load(imgUri)
-            .apply(
-                RequestOptions()
-                .placeholder(R.drawable.loading_animation)
-                .error(R.drawable.ic_broken_image))
-            .into(imgView)
-    }
+// is this used?
+//@BindingAdapter("imageUrl")
+//fun bindImage(imgView: ImageView, imgUrl: String?) {
+//    imgUrl?.let {
+//        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
+//        Glide.with(imgView.context)
+//            .load(imgUri)
+//            .apply(
+//                RequestOptions()
+//                .placeholder(R.drawable.loading_animation)
+//                .error(R.drawable.ic_broken_image))
+//            .into(imgView)
+//    }
+//}
+
+
+@BindingAdapter("pictureOfDayDescription")
+fun bindPictureOfDayDescription(imageView: ImageView, title:String) {
+    imageView.context.getString(R.string.nasa_picture_of_day_content_description_format, title)
 }
+
 
 //@BindingAdapter("marsApiStatus")
 //fun bindStatus(statusImageView: ImageView, status: MainViewModel.PictureOfDayStatus?) {
@@ -81,3 +100,49 @@ fun bindImage(imgView: ImageView, imgUrl: String?) {
 //        }
 //    }
 //}
+
+//
+//@BindingAdapter("apodImageUrl")
+//fun bindApodImage(imgView: ImageView, imgUrl: String?) {
+//    imgUrl?.let {
+//        // Load the image in the background using Coil.
+//        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
+//        imgView.load(imgUri) {
+//            placeholder(R.drawable.loading_animation)
+//            error(R.drawable.ic_broken_image)
+//        }
+//    }
+//}
+//@BindingAdapter("imageUrl")
+//fun bindImage(imgView: ImageView, imgUrl: String?) {
+//    imgUrl?.let {
+//        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
+//        Glide.with(imgView.context)
+//            .load(imgUri)
+//            .apply(
+//                RequestOptions()
+//                    .placeholder(R.drawable.loading_animation)
+//                    .error(R.drawable.ic_broken_image))
+//            .into(imgView)
+//    }
+//}
+
+/*
+@BindingAdapter("listData")
+fun bindRecyclerView(
+    recyclerView: RecyclerView,
+    data: List<Asteroid>?
+) {
+    val adapter = recyclerView.adapter as MainAsteroidAdapter
+    adapter.submitList(data)
+}
+@BindingAdapter("listData")
+fun bindRecyclerView(recyclerView: RecyclerView, data: List<DatabaseAsteroid>?) {
+    val adapter = recyclerView.adapter as? AsteroidAdapter
+    adapter?.submitList(data)
+}
+*/
+
+
+
+
